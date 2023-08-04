ed0f84b36588f1ec5d9b3f0cd8c23332c5f6b2a1
Avshalom-Ts <avshalomz.ts@gmail.com>
2023-08-04 17:09:26 +0300

    Init

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'ang-fire-1b201',
+    appId: '1:543600466644:web:05e9db300a44c8939eb941',
+    storageBucket: 'ang-fire-1b201.appspot.com',
+    apiKey: 'AIzaSyCHn8zoqizS3o6uEQ1fFO2pJeqxOsBWJ70',
+    authDomain: 'ang-fire-1b201.firebaseapp.com',
+    messagingSenderId: '543600466644',
+  },
+};
