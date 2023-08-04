58f6d09fe706f01a3412c395b9765b0cd8921e63
Avshalom-Ts <avshalomz.ts@gmail.com>
2023-08-04 17:46:00 +0300

    Geting all the users data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Firestore,collection,addDoc } from '@angular/fire/firestore';
+import { Firestore,collection,addDoc,collectionData } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +8,9 @@ import { Firestore,collection,addDoc } from '@angular/fire/firestore';
 })
 export class AppComponent {
 
-  constructor(private firestore:Firestore){}
+  constructor(private firestore: Firestore) {
+    this.getData();
+  }
 
   addData(f:any) {
     // console.log(f.value);
@@ -20,4 +22,12 @@ export class AppComponent {
         console.log(err);
     })
   };
+
+  getData() {
+    const collectionInstance = collection(this.firestore, 'users');
+    collectionData(collectionInstance).subscribe(val => {
+      console.log(val);
+    })
+    
+  }
 }
