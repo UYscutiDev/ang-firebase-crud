018cc12a7699267ba233c482be57693ebe376594
Avshalom-Ts <avshalomz.ts@gmail.com>
2023-08-04 18:17:38 +0300

    Updated user doc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Firestore,collection,addDoc,collectionData } from '@angular/fire/firestore';
+import { Firestore, collection, addDoc, collectionData, doc, updateDoc } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -8,30 +8,47 @@ import { Observable } from 'rxjs';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  userData!:Observable<any>
+  userData!: Observable<any>;
+  allUsers: any;
 
   constructor(private firestore: Firestore) {
     this.getData();
   }
 
-  addData(f:any) {
+  addData(f: any) {
     // console.log(f.value);
     const collectionInstance = collection(this.firestore, 'users');
-    addDoc(collectionInstance, f.value).then(() => {
-      console.log('Data saved successfully');
-    })
+    addDoc(collectionInstance, f.value)
+      .then(() => {
+        console.log('Data saved successfully');
+      })
       .catch((err) => {
         console.log(err);
-    })
-  };
+      });
+  }
 
   getData() {
     const collectionInstance = collection(this.firestore, 'users');
-    collectionData(collectionInstance).subscribe(val => {
+    collectionData(collectionInstance, { idField: 'id' }).subscribe((val) => {
       console.log(val);
-    })
+      this.allUsers = val;
+    });
     this.userData = collectionData(collectionInstance);
   }
 
-
+  updateData(id: any) {
+    // console.log(id);
+    const docInstance = doc(this.firestore, 'users', id);
+    const updateData = {
+      name:'updatedName'
+    }
+    updateDoc(docInstance, updateData)
+      .then(() => {
+        console.log('Data updated');
+      })
+      .catch((err) => {
+        console.log(err);
+    })
+    console.log(docInstance)
+  }
 }
